f38f896eed0db4aac32bfc61fd2ca9d431a3a209
Jim Tkaczyk <jimtkaczyk@gmail.com>
2020-01-08 12:02:14 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "US East (N. Virginia)",
+    BUCKET: "notes-testapp2-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://l9oh6vbooe.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_eaU3daEvx",
+    APP_CLIENT_ID: "5tb2tv17d60n3r56g20dhoes6",
+    IDENTITY_POOL_ID: "us-east-2:7e64a595-1dcb-4eb7-be54-c7823d99d499"
+  }
+};
